33d9038797841d9c237332b875068cf9385c1584
NetAcad DEVASC <akashghimire819@gmail.com>
2021-10-21 01:22:22 +0000

    Added Delete Function

--- add100RandomBooks.py
+++ add100RandomBooks.py
@@ -21,6 +21,7 @@ def getAuthToken():
         raise Exception(f"Status code {r.status_code} and text {r.text}, while trying to Auth.")
 
 def addBook(book, apiKey):
+    num=str(id)
     r = requests.post(
         f"{APIHOST}/api/v1/books", 
         headers = {
@@ -33,16 +34,31 @@ def addBook(book, apiKey):
         print(f"Book {book} added.")
     else:
         raise Exception(f"Error code {r.status_code} and text {r.text}, while trying to add book {book}.")
-
+        
+        
+def delBook(book, apiKey,id):
+    id=str(id)
+    r = requests.delete(
+        f"{APIHOST}/api/v1/books/id", 
+        headers = {
+            "Content-type": "application/json",
+            "X-API-Key": apiKey
+            },
+        data = json.dumps(book)
+    )
+    if r.status_code == 200:
+        print(f"Book {book} deleted.")
+    else:
+        raise Exception(f"Error code {r.status_code} and text {r.text}, while trying to add book {book}.")
+        
 # Get the Auth Token Key
 apiKey = getAuthToken()
 
 # Using the faker module, generate random "fake" books
 fake = Faker()
-for i in range(4, 30):
-    fakeTitle = fake.catch_phrase()
-    fakeAuthor = fake.name()
-    fakeISBN = fake.isbn13()
-    book = {"id":i, "title": fakeTitle, "author": fakeAuthor, "isbn": fakeISBN}
+for i in range(5):
     # add the new random "fake" book using the API
-    addBook(book, apiKey) 
+    delBook(book, apiKey,id) 
+    
+ for i in range(-5,-1): 
+    delBook(book, apiKey,id)  
